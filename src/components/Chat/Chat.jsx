769161fe058d91527ac07c9350fa8fa2e54d1d71
Matvey <matvey.w2015@mail.ru>
2020-09-11 16:29:55 +0400

    add ability to send message from input

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -3,8 +3,24 @@ import classes from './Chat.module.css'
 import {SearchOutlined, AttachFile, MoreVert, InsertEmoticon, Mic} from '@material-ui/icons'
 import { Avatar, IconButton } from '@material-ui/core'
 import Message from './Message/Message'
+import axios from './axios'
 
 function Chat({ messages }) {
+
+  const [input, setInput] = useState('')
+
+    const sendMessage = async e => {
+      e.preventDefault()
+
+      await axios.post('/messages/new', {
+        message:input,
+        name: 'Demo Name', // make auth
+        timestamp: 'Just Now',
+        received: false // make auth
+      })
+      setInput('')
+    }
+
     return (
       <div className={classes.chat}>
         <div className={classes.header}>
@@ -36,14 +52,14 @@ function Chat({ messages }) {
           </IconButton>
           <form>
             <input
-              // value={input}
-              // onChange={(e) => SettingsInputAntenna(e.target.value)}
+              value={input}
+              onChange={(e) => SettingsInput(e.target.value)}
               placeholder="Type a message"
               type="text"
             />
             <button
               type="submit"
-              // onClick={sendMessage}
+              onClick={sendMessage}
             >
               Send a message
             </button>
